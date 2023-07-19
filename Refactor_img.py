740d028e41cf0187658ce39ad5bfa7e05e6ee58b
Ivanalq <aleqnikov@gmail.com>
2023-07-19 11:50:34 +0300

    pre-version-1

--- Refactor_img.py
+++ Refactor_img.py
@@ -1,21 +1,33 @@
+from urllib import parse
 from PIL import Image
 
-
-def refactor_img(list_images):
+def refactor_img(list_images, directory_exit):
     for name_img in list_images:
         image = Image.open(name_img)
         width, height = image.size
         if width/height >= 1.3:
             pattern = Image.open('patterns/pattern_horizontal.png')
             image.paste(pattern.resize((width, height)), (0, 0), pattern.resize((width, height)))
-            image.save(name_img[:name_img.find('.')] + '_paste.png')
+            unquoted_url = parse.unquote(name_img)
+            path = parse.urlparse(unquoted_url).path
+            name_cart = path.rstrip("/").split("/")[-1]
+            image.save(directory_exit + name_cart[:name_cart.find('.')] + '-wt.png')
 
         if width/height <= 0.7:
             pattern = Image.open('patterns/pattern_vertical.png')
             image.paste(pattern.resize((width, height)), (0, 0), pattern.resize((width, height)))
-            image.save(name_img[:name_img.find('.')] + '_paste.png')
+            unquoted_url = parse.unquote(name_img)
+            path = parse.urlparse(unquoted_url).path
+            name_cart = path.rstrip("/").split("/")[-1]
+            image.save(directory_exit + name_cart[:name_cart.find('.')] + '-wt.png')
 
         if 0.7 < width/height < 1.3:
             pattern = Image.open('patterns/pattern_square.png')
             image.paste(pattern.resize((width, height)), (0, 0), pattern.resize((width, height)))
-            image.save(name_img[:name_img.find('.')] + '_paste.png')
+            unquoted_url = parse.unquote(name_img)
+            path = parse.urlparse(unquoted_url).path
+            name_cart = path.rstrip("/").split("/")[-1]
+            image.save(directory_exit + name_cart[:name_cart.find('.')] + '-wt.png')
+
+
+
--- main.py
+++ main.py
@@ -1,4 +1,41 @@
 from Refactor_img import refactor_img
+from tkinter import *
+from tkinter import filedialog
+from tkinter import ttk
+
+import tkinter as tk
+
+import tkinter.filedialog as fd
 
 
-root = tk
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.directiry = ()
+        self.res = ()
+        btn_file = tk.Button(self, text='Выберите файл', command=self.choise_file)
+        btn_dir = tk.Button(self, text='Выберите Папку', command=self.choise_directiry)
+        btn_sub = tk.Button(self, text='Наложить водяные знаки', command=self.start_wm)
+        btn_file.pack(padx=60, pady=10)
+        btn_dir.pack(padx=60, pady=10)
+        btn_sub.pack(padx=60, pady=10)
+
+
+    def choise_file(self):
+        self.res = filedialog.askopenfilename(multiple=1)
+        return self.res
+
+
+    def choise_directiry(self):
+        self.directiry = fd.askdirectory(title='Открыть папку', initialdir='/') + '/'
+
+    def start_wm(self):
+        refactor_img(list_images=self.res, directory_exit=self.directiry)
+
+
+
+
+if __name__ == "__main__":
+    app = App()
+    app.geometry('1000x1000')
+    app.mainloop()
