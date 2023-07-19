d211af6f622abd2d2bca00d1c485af1b551352c1
Ivanalq <aleqnikov@gmail.com>
2023-07-19 12:50:58 +0300

    finaly_version

--- Refactor_img.py
+++ Refactor_img.py
@@ -1,33 +1,36 @@
 from urllib import parse
 from PIL import Image
 
+"""У каждого изображения есть какая либо ориентация сторон, например 16:9, и что бы определить то, какой
+    формы изображение мы узнаем коэфицент сторон, если он больше 1.3 то это горизонтальное изображение, если
+    меньше 0.7 то это вертикальное изображение, а между ними квадратное, таким образом мы подбираем наилучший
+    шаблон под конкретное изображение с помощью данной функции"""\
+
+
+def find_pattern(widthheight):
+    if widthheight >= 1.3:
+        return Image.open('patterns/pattern_horizontal.png')
+    elif widthheight <= 0.7:
+        return Image.open('patterns/pattern_vertical.png')
+    elif  0.7 < widthheight < 1.3:
+        return Image.open('patterns/pattern_square.png')
+
+
 def refactor_img(list_images, directory_exit):
-    for name_img in list_images:
-        image = Image.open(name_img)
-        width, height = image.size
-        if width/height >= 1.3:
-            pattern = Image.open('patterns/pattern_horizontal.png')
-            image.paste(pattern.resize((width, height)), (0, 0), pattern.resize((width, height)))
-            unquoted_url = parse.unquote(name_img)
-            path = parse.urlparse(unquoted_url).path
-            name_cart = path.rstrip("/").split("/")[-1]
-            image.save(directory_exit + name_cart[:name_cart.find('.')] + '-wt.png')
+    for name_img in list_images: #Получает все ссылки изображений
+        image = Image.open(name_img) #Открывает изображение
+        width, height = image.size  #Узнает его высоту и ширину
 
-        if width/height <= 0.7:
-            pattern = Image.open('patterns/pattern_vertical.png')
-            image.paste(pattern.resize((width, height)), (0, 0), pattern.resize((width, height)))
-            unquoted_url = parse.unquote(name_img)
-            path = parse.urlparse(unquoted_url).path
-            name_cart = path.rstrip("/").split("/")[-1]
-            image.save(directory_exit + name_cart[:name_cart.find('.')] + '-wt.png')
+        widthheight = width/height #высчитывает коэфицент высоты на ширину
+        pattern = find_pattern(widthheight=widthheight) #Получает подходящий паттерн
+        image.paste(pattern.resize((width, height)), (0, 0), pattern.resize((width, height))) #Накладывает одно изображение на другое
+        unquoted_url = parse.unquote(name_img)
+        path = parse.urlparse(unquoted_url).path
+        name_cart = path.rstrip("/").split("/")[-1] #Получает имя файла
+        image.save(directory_exit + name_cart[:name_cart.find('.')] + '-wt.png') #Сохраняет файл
 
-        if 0.7 < width/height < 1.3:
-            pattern = Image.open('patterns/pattern_square.png')
-            image.paste(pattern.resize((width, height)), (0, 0), pattern.resize((width, height)))
-            unquoted_url = parse.unquote(name_img)
-            path = parse.urlparse(unquoted_url).path
-            name_cart = path.rstrip("/").split("/")[-1]
-            image.save(directory_exit + name_cart[:name_cart.find('.')] + '-wt.png')
+
+
 
 
 
--- main.py
+++ main.py
@@ -1,41 +1,31 @@
 from Refactor_img import refactor_img
-from tkinter import *
 from tkinter import filedialog
-from tkinter import ttk
-
 import tkinter as tk
-
 import tkinter.filedialog as fd
 
 
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.directiry = ()
-        self.res = ()
         btn_file = tk.Button(self, text='Выберите файл', command=self.choise_file)
         btn_dir = tk.Button(self, text='Выберите Папку', command=self.choise_directiry)
         btn_sub = tk.Button(self, text='Наложить водяные знаки', command=self.start_wm)
-        btn_file.pack(padx=60, pady=10)
+        btn_file.pack(padx=60, pady=10) 
         btn_dir.pack(padx=60, pady=10)
         btn_sub.pack(padx=60, pady=10)
 
-
-    def choise_file(self):
+    def choise_file(self): #Функция выбора фалов
         self.res = filedialog.askopenfilename(multiple=1)
-        return self.res
 
-
-    def choise_directiry(self):
+    def choise_directiry(self): #Функция выбора папки
         self.directiry = fd.askdirectory(title='Открыть папку', initialdir='/') + '/'
 
-    def start_wm(self):
+    def start_wm(self): #Функция, которая начинает процесс наложения водяных знаков
         refactor_img(list_images=self.res, directory_exit=self.directiry)
-
-
 
 
 if __name__ == "__main__":
     app = App()
-    app.geometry('1000x1000')
+    app.title('Добавление Водяных Знаков')
+    app.resizable(width=False, height=False)
     app.mainloop()
